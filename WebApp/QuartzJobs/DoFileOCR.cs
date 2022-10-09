c956bf85f91323581d292ac951e9b16a0db10681
majid <majid.shafaee@gmail.com>
2022-10-09 19:43:57 +0330

    Add_1 DoOCR Job

--- WebApp/QuartzJobs/DoFileOCR.cs
+++ WebApp/QuartzJobs/DoFileOCR.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApp.QuartzJobs;
+﻿
+
+namespace WebApp.QuartzJobs;
 
 public class DoFileOCR : IJob
 {
@@ -13,7 +15,23 @@ public class DoFileOCR : IJob
     public async Task Execute(IJobExecutionContext context)
     {
         _logger.LogInformation("Starting ReadProjectExcleFile ");
-        var filesToOCR=await _projectService.Get5FilesToOCR();  
+        var filesToOCR=await _projectService.Get5FilesToOCR(); 
+        foreach(var file in filesToOCR)
+        {
+            TesseractPersianOCR ocr = new TesseractPersianOCR(file.PdfFileUrl, new TesseractPersianOCR.LoggerCallback(LoggerCallback), new TesseractPersianOCR.DbCallback(DbCallback));
+            Thread t1 = new Thread(new ThreadStart(ocr.DoOCR));
+            t1.Start();
+        }
+    }
+
+    public void LoggerCallback(string log)
+    {
+        _logger.LogError("{@appName}. error in doing OCR.{@errorMessage}", "OCR", log);
+    }
+
+    public void DbCallback(string ocrText)
+    {
+         _logger.LogError("{@appName}. error in doing OCR.{@errorMessage}", "OCR", ocrText);
     }
 }
 
--- WebApp/globalusings.cs
+++ WebApp/globalusings.cs
@@ -2,6 +2,7 @@
 global using DAL.Services;
 global using WebApp.Common;
 global using WebApp.QuartzJobs;
+global using OCR;
 
 global using Microsoft.EntityFrameworkCore;
 global using System.Data;
